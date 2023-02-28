0ad28eb6991d080c1b00f1d5209c4fae674eb2f2
DamianMk <damianmajka4@gmail.com>
2023-03-01 00:03:51 +0100

    Utworzenie modeli i zarejestrowanie ich w admin.py.

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from reservations.models import RoomStandard
+from reservations.models import Room
+from reservations.models import Facility
+from reservations.models import RoomFacility
+from reservations.models import HotelGuest
+from reservations.models import Reservation
+from reservations.models import Extras
+from reservations.models import ReservationExtras
+
 
 # Register your models here.
+
+
+admin.site.register(RoomStandard)
+admin.site.register(Room)
+admin.site.register(Facility)
+admin.site.register(RoomFacility)
+admin.site.register(HotelGuest)
+admin.site.register(Reservation)
+admin.site.register(Extras)
+admin.site.register(ReservationExtras)
+
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class RoomStandard(models.Model):
+    standard_name = models.CharField(max_length=50)
+    price = models.SmallIntegerField(validators=[MinValueValidator(0)])
+    description = models.TextField(max_length=300, default='')
+
+    def __str__(self):
+        return f'{self.standard_name}'
+
+
+class Room(models.Model):
+    room_number = models.SmallIntegerField(validators=[MinValueValidator(0)])
+    room_area = models.SmallIntegerField(validators=[MinValueValidator(0)])
+    room_standard_id = models.ForeignKey(
+        RoomStandard, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return f'{self.room_number}'
+
+
+class Facility(models.Model):
+    facility_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.facility_name}'
+
+
+class RoomFacility(models.Model):
+    room_id = models.ForeignKey(
+        Room, on_delete=models.SET_NULL, null=True
+    )
+    facility_id = models.ForeignKey(
+        Facility, on_delete=models.SET_NULL, null=True
+    )
+
+
+class HotelGuest(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+
+class Reservation(models.Model):
+    reservation_number = models.CharField(max_length=100)
+    booking_date = models.DateField(auto_now_add=True, editable=False)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    number_of_guests = models.SmallIntegerField()
+    hotel_guest_id = models.ForeignKey(
+        HotelGuest, on_delete=models.SET_NULL, null=True
+    )
+    room_id = models.ForeignKey(
+        Room, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return f'{self.reservation_number}'
+
+
+class Extras(models.Model):
+    extras_name = models.CharField(max_length=50)
+    price = models.SmallIntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f'{self.extras_name}'
+
+
+class ReservationExtras(models.Model):
+    reservation_id = models.ForeignKey(
+        Reservation, on_delete=models.SET_NULL, null=True
+    )
+    extras_id = models.ForeignKey(
+        Extras, on_delete=models.SET_NULL, null=True
+    )
+
+
+
+
+
+
